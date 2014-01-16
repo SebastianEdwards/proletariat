08034f43de85bd15b8580d35f8ffb9bba119fe6e
Sebastian Edwards <me@sebastianedwards.co.nz>
2014-01-16 21:13:58 +1300

    Make rake task more compatible with foreman.

--- lib/proletariat/tasks.rb
+++ lib/proletariat/tasks.rb
@@ -4,7 +4,13 @@ task :environment
 
 namespace :proletariat do
   desc 'Start background processing'
-  task :run => :environment do
-    Proletariat.run
+  task run: :environment do
+    STDOUT.sync = true
+
+    Proletariat.run!
+
+    at_exit { Proletariat.stop }
+
+    sleep
   end
 end
