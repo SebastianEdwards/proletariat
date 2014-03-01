8e0e566ba907e505bc5b9610233cb877df560e36
Sebastian Edwards <me@sebastianedwards.co.nz>
2014-03-02 12:19:06 +1300

    Add test for case of failing worker.

--- spec/lib/proletariat_spec.rb
+++ spec/lib/proletariat_spec.rb
@@ -24,10 +24,15 @@ class PongWorker < Proletariat::Worker
   listen_on :pong
 
   class << self
+    attr_accessor :fail_mode
     attr_accessor :ponged
   end
 
   def work(message, routing_key, headers)
+    if self.class.fail_mode == true
+      fail 'Error' unless headers['failures'] == 2
+    end
+
     self.class.ponged = true
 
     log 'PONG'
@@ -39,18 +44,38 @@ class PongWorker < Proletariat::Worker
 end
 
 describe Proletariat do
-  it 'should roughly work' do
+  before do
     Proletariat.configure do
       config.logger         = Logger.new('/dev/null')
       config.worker_classes = [PingWorker, PongWorker]
     end
 
+    PongWorker.fail_mode = false
+
+    Proletariat.purge
     Proletariat.run!
     sleep 2
-    Proletariat.publish 'ping', ''
-    sleep 3
+  end
+
+  after do
     Proletariat.stop
     Proletariat.purge
+    PingWorker.pinged = false
+    PongWorker.ponged = false
+  end
+
+  it 'should roughly work' do
+    Proletariat.publish 'ping', ''
+    sleep 5
+
+    expect(PingWorker.pinged).to be_truthy
+    expect(PongWorker.ponged).to be_truthy
+  end
+
+  it 'should work in error conditions' do
+    PongWorker.fail_mode = true
+    Proletariat.publish 'ping', ''
+    sleep 10
 
     expect(PingWorker.pinged).to be_truthy
     expect(PongWorker.ponged).to be_truthy
